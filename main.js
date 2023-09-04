fef98a7b56d2f2bf13a168c27deeb77495677d09
msvarma10001@gmail.com <msvarma10001@gmail.com>
2023-09-04 09:51:31 +0530

    Genrated numbers using loop now remove hard coding

--- main.js
+++ main.js
@@ -1,32 +1,21 @@
-var numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90];
-
-
+var numbersArray = [];
 let num = document.querySelector("#num")
-
 let button = document.querySelector("button")
-
 let audio = new Audio("assets/play.wav")
-
 let showArr = []
-
 let buttonContainer = document.querySelector(".button-container")
-
 let card = document.querySelector(".card")
 
 
-
+//click event.
 button.addEventListener("click", () => {
   buttonContainer.innerHTML = ""
   change()
-
-
 })
 
 
-
-
+// Change value and store.
 let change = () => {
-
 
   if (numbersArray == "") {
     num.textContent = "OVER"
@@ -60,3 +49,15 @@ let change = () => {
 
   }
 }
+
+
+
+// Genrate All Numbers first.
+let numGen = () => {
+
+  for (var i = 1; i <= 90; i++) {
+    numbersArray.push(i)
+  }
+}
+
+numGen()
